3628a703eef1f3ee04a59caafc34b0b31b32665a
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2022-06-24 10:17:48 -0300

    feat: titular deve ser maior de idade

--- src/main/java/br/ufpe/cin/CadastroConta.java
+++ src/main/java/br/ufpe/cin/CadastroConta.java
@@ -4,6 +4,9 @@ import java.util.UUID;
 
 public class CadastroConta {
   public Conta criarConta(Pessoa titular) {
+    if (!titular.isMaiorIdade()) {
+      throw new IllegalArgumentException("O titular da conta deve ser maior de idade!");
+    }
     final Conta conta = new Conta(UUID.randomUUID(), titular);
     return conta;
   }
